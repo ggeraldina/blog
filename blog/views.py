00379e3359a1892ec0e6f3db9a5c9d8a8b99f6f6
Geraldina (laptop) <grushevskaya.d.v@yandex.ru>
2020-02-28 23:56:34 +0300

    Added 404 page

--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,6 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     date = { 'posts': posts }
     return render(request, 'blog/post_list.html', date)
+
+def page_not_found(request, exception):
+    return render(request, 'blog/page_not_found.html', status=404)
