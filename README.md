d14c112a3d35a8b45054de154ac90d9e86fe1131
Грушевская Дарья Владимировна <grushevskaya.d.v@yandex.ru>
2020-02-28 10:27:11 +0300

    Fixed settings.py

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
  ## Создание
- 1. Add requirements.txt
+ 1. requirements.txt
  2. django-admin startproject name_project .
+ 3. Настройка
+    
+    * TIME_ZONE = 'Europe/Moscow'
+    * LANGUAGE_CODE = 'ru-ru'
+    * STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    * ALLOWED_HOSTS = ['127.0.0.1', 'site.com']
--- blog_project/settings.py
+++ blog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h@^fj_%%lf5^0#aaelx4!rfe7o8le3@+vu9nr*xur!2#1t3g#_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
