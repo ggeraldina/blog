72bb666447016a8f9dc89d1acdb3c29b1f62c38d
Geraldina (laptop) <grushevskaya.d.v@yandex.ru>
2020-02-29 09:31:43 +0300

    Added links to other pages

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -6,6 +6,11 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     date = { 'posts': posts }
     return render(request, 'blog/post_list.html', date)
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    date = { 'post': post }
+    return render(request, 'blog/post_detail.html', date)
 
 def page_not_found(request, exception):
     return render(request, 'blog/page_not_found.html', status=404)
