a0d588fb1eb0f6a7f610f2f472017028af95fbc5
Geraldina (laptop) <grushevskaya.d.v@yandex.ru>
2020-02-29 12:58:53 +0300

    Added a page with a form

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    date = { 'posts': posts }
-    return render(request, 'blog/post_list.html', date)
+    data = {'posts': posts}
+    return render(request, 'blog/post_list.html', data)
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    date = { 'post': post }
-    return render(request, 'blog/post_detail.html', date)
+    data = {'post': post}
+    return render(request, 'blog/post_detail.html', data)
+
+def post_new(request):
+    form = PostForm()
+    data = {'form': form}
+    return render(request, 'blog/post_edit.html', data)
 
 def page_not_found(request, exception):
     return render(request, 'blog/page_not_found.html', status=404)
