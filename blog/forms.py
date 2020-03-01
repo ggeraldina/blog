1dc845cfde3dd3b4fd230e872613ef4a9abe196b
Geraldina (laptop) <grushevskaya.d.v@yandex.ru>
2020-03-01 14:52:56 +0300

    Fixed the post form

--- blog/forms.py
+++ blog/forms.py
@@ -5,3 +5,7 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text')
+        labels = {
+            'title': 'Название',
+            'text': 'Текст',
+        }
